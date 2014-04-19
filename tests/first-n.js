09352e53cedce7e21d6e3e8bef7b00dd67746ccf
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-04-19 14:39:03 +0000

    Use mocha/chai for testing

--- tests/first-n.js
+++ tests/first-n.js
@@ -1,25 +1,23 @@
-var tape = require('tape');
+var test = require('chai').assert;
 var first = require('../index');
 
-tape('returns first item of array', function (test) {
-  test.plan(1);
-  test.equals(first([6,0,0,8,14]), 6);
-});
+describe('first', function() {
+  it('returns first item of array', function () {
+    test.strictEqual(first([6,0,0,8,14]), 6);
+  });
 
-tape('returns first array of array', function (test) {
-  test.plan(1);
-  test.equals(first([[6,16],0,8,14])[1], 16);
-});
+  it('returns first array of array', function () {
+    test.strictEqual(first([[6,16],0,8,14])[1], 16);
+  });
 
-tape('returns first n items of array', function (test) {
-  test.plan(2);
-  test.equals(first(['Hi', 'World', 1337], 2).join(' '), 'Hi World');
-  test.equals(first(['Hi', 'World', 1337], 3).join(' '), 'Hi World 1337');
-});
+  it('returns first n items of array', function () {
+    test.strictEqual(first(['Hi', 'World', 1337], 2).join(' '), 'Hi World');
+    test.strictEqual(first(['Hi', 'World', 1337], 3).join(' '), 'Hi World 1337');
+  });
 
-tape('returns items of array while callback return truthy', function (test) {
-  test.plan(1);
-  test.deepEqual(first([1, 52, 89, 1337], function (num) {
-    return num < 1337;
-  }), [1, 52, 89]);
+  it('returns items of array while callback return truthy', function () {
+    test.deepEqual(first([1, 52, 89, 1337], function (num) {
+      return num < 1337;
+    }), [1, 52, 89]);
+  });
 });
